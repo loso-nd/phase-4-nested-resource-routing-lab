95b5eff7c5ece74dfd1e407dc8778c05320e8ace
Nahum <losond8@gmail.com>
2021-07-29 15:23:47 -0700

    commit tree 75e706c01bfec39c8df0deb7b00693036105d363

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # resources :items, only: [:index]
+  # resources :users, only: [:show]
+
+  #GET /users/:user_id/items
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :show, :create]
+  end
+
 end
+
+# GET   /dog_houses/:dog_house_id/reviews               reviews#index
+# GET   /dog_houses/:dog_house_id/reviews/:id           reviews#show 
